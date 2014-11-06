459375eb0e3dbe713a28be1dfc19f6dd09c38e5a
Filip Bartek <filip.bartek@hotmail.com>
2014-11-06 20:04:37 +0100

    Add initial sketchbook "sensors.ino"

---
+++ sensors/sensors.ino
@@ -0,0 +1,72 @@
+#include <Servo.h>
+
+Servo servo_l; // 0: back, 180: forward
+Servo servo_r; // 180: back, 0: forward
+
+//int sensor = A0;
+// 0: light returns; white surface
+// 1023: light gets lost; black surface
+
+int sensor_l_pin = 11;
+int sensor_r_pin = 10;
+int sensor_c_pin = 9;
+// 0: light returns; white surface
+// 1: light gets lost; black surface
+
+void setup() {
+  Serial.begin(9600);
+  servo_l.attach(13);
+  servo_r.attach(12);
+  servo_l.write(90);
+  servo_r.write(90);
+  
+  pinMode(sensor_l_pin, INPUT);
+  pinMode(sensor_r_pin, INPUT);
+  pinMode(sensor_c_pin, INPUT);
+}
+
+// left, right:
+// 0: stop
+// 90: full forward
+// -90: full back
+void run(long left, long right) {
+  long power_l = map(left, -90, 90, 0, 180);
+  long power_r = map(right, -90, 90, 180, 0);
+  Serial.print("power_l: ");
+  Serial.println(power_l);
+  Serial.print("power_r: ");
+  Serial.println(power_r);
+  servo_l.write(power_l);
+  servo_r.write(power_r);
+}
+
+void loop() {
+//  int val = analogRead(sensor);
+//  Serial.print(val);
+//  int power = map(val, 0, 1023, 90, 104);
+//  Serial.print(" ");
+//  Serial.print(power);
+//  Serial.println("");
+//  servo_l.write(power);
+//  servo_r.write(power);
+  int val_l = digitalRead(sensor_l_pin);
+  int val_r = digitalRead(sensor_r_pin);
+  int val_c = digitalRead(sensor_c_pin);
+  long left = 90;
+  long right = 90;
+  if (!val_l && val_r) {
+    // go right
+    right = 0;
+  }
+  if (!val_r && val_l) {
+    // go left
+    left = 0;
+  }
+  Serial.print(left);
+  Serial.print(" ");
+  Serial.print(right);
+  Serial.println("");
+  run(left, right);
+  delay(1);
+}
+
