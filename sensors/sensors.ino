3fd75887ed4f72c9cb740b7899891bc7db060ca1
Filip Bartek <filip.bartek@hotmail.com>
2014-11-20 19:24:33 +0100

    Implement better behavior for 5 sensors (new robot)

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -3,20 +3,31 @@
 
 Servo servo_l; // 0: back, 180: forward
 Servo servo_r; // 180: back, 0: forward
+ 
+// sensors: 7 11 9 10 8
+// button: 6
 
 const int sensor_l_pin = 11;
 const int sensor_r_pin = 10;
 const int sensor_c_pin = 9;
+const int sensor_ll_pin = 7;
+const int sensor_rr_pin = 8;
 // 0: light returns; white surface
 // 1: light gets lost; black surface
 
-const int button_pin = 8;
+const int button_pin = 6;
 
+// Mode:
+boolean wait_for_button = true;
 boolean dir = true;
+boolean after_junction = false;
 // true: right
 // false: left
 
 int val_button_prev = 1;
+
+boolean turn_l_prev = false;
+boolean turn_r_prev = false;
 
 void setup() {
   // Initialize debug output channel
@@ -49,70 +60,113 @@ void loop() {
   const int val_l = digitalRead(sensor_l_pin);
   const int val_r = digitalRead(sensor_r_pin);
   const int val_c = digitalRead(sensor_c_pin);
-  // default: go back slowly
-  long left = -5;
-  long right = -5;
-  if (val_c && !val_l && !val_r) {
-    // go forward
-    left = 90;
-    right = 90;
-  }
-  if (!val_l && val_r) {
-    // go right
-    left = 90;
-    if (val_c) {
-      if (!dir) {
-        // forward
-        right = 90;
-      } else {
-        // right weak
-        right = 0;
-      }
+  const int val_ll = digitalRead(sensor_ll_pin);
+  const int val_rr = digitalRead(sensor_rr_pin);
+  
+  const boolean turn_l = val_ll && !val_rr;
+  const boolean turn_r = val_rr && !val_ll;
+  
+  if (turn_l && !turn_l_prev) {
+    // TODO: Consider considering dir here.
+    if (after_junction) {
+      after_junction = false;
     } else {
-      // right strong
-      right = -90;
+      dir = false;
+      after_junction = true;
     }
   }
-  if (!val_r && val_l) {
-    // go left
-    right = 90;
-    if (val_c) {
-      if (dir) {
-        // forward
-        left = 90;
+  
+  if (turn_r && !turn_r_prev) {
+    if (after_junction) {
+      after_junction = false;
+    } else {
+      dir = true;
+      after_junction = true;
+    }
+  }
+  
+  turn_l_prev = turn_l;
+  turn_r_prev = turn_r;
+  
+  // TODO: Implement dir oblivious mode for period after `after_junction` is set to false.
+  // (?)
+  
+  if (val_ll && val_rr && !wait_for_button) {
+    wait_for_button = true;
+    run(0, 0);
+  }
+  
+  if (wait_for_button) {
+    // Wait
+    
+    const int val_button = digitalRead(button_pin);
+    // 0: pressed
+    // 1: released
+    
+    if (!val_button) {
+      // Button pressed
+      // Wait is over
+      wait_for_button = false;
+      after_junction = false;
+      // TODO: Add timeout
+    }
+  }
+  if (!wait_for_button) {
+    // Run
+    // default: go back slowly
+    long left = -5;
+    long right = -5;
+    if (val_c && !val_l && !val_r) {
+      // go forward
+      left = 90;
+      right = 90;
+    }
+    if (!val_l && val_r) {
+      // go right
+      left = 90;
+      if (val_c) {
+        if (!dir) {
+          // forward
+          right = 90;
+        } else {
+          // right weak
+          right = 0;
+        }
       } else {
-        // left weak
+        // right strong
+        right = -90;
+      }
+    }
+    if (!val_r && val_l) {
+      // go left
+      right = 90;
+      if (val_c) {
+        if (dir) {
+          // forward
+          left = 90;
+        } else {
+          // left weak
+          left = 0;
+        }
+      } else {
+        // left strong
+        left = -90;
+      }
+    }
+    if (val_l && val_r) {
+      // choose by `dir`
+      if (dir) {
+        // go right weak
+        left = 90;
+        right = 0;
+      } else {
+        // go left weak
+        right = 90;
         left = 0;
       }
-    } else {
-      // left strong
-      left = -90;
     }
+    run(left, right);
   }
-  if (val_l && val_r) {
-    // choose by `dir`
-    if (dir) {
-      // go right weak
-      left = 90;
-      right = 0;
-    } else {
-      // go left weak
-      right = 90;
-      left = 0;
-    }
-  }
-  run(left, right);
-  
-  int val_button = digitalRead(button_pin);
-  // 0: pressed
-  // 1: released
-  
-  // Flip `dir` if button was pressed in this frame.
-  if (!val_button && val_button_prev) {
-    // The button was pressed just now
-    dir = !dir;
-  }
-  val_button_prev = val_button;
   
   Serial.println(dir);
 }
