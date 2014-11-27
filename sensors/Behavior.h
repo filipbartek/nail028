661400085c64352389581ae5c25d19e54a717505
Filip Bartek <filip.bartek@hotmail.com>
2014-11-27 19:18:12 +0100

    Add Behavior.h with a stub of Behavior class

---
+++ sensors/Behavior.h
@@ -0,0 +1,33 @@
+#include "Runner.h"
+
+struct SensorPins {
+  int ll;
+  int l;
+  int c;
+  int r;
+  int rr;
+};
+
+class Behavior {
+public:
+  Behavior(const SensorPins& sensor_pins, const int& button_pin, Runner& runner)
+    : sensor_pins(sensor_pins), button_pin(button_pin), runner(runner) {}
+  void tick() = 0;
+private:
+  const SensorPins& sensor_pins;
+  const int& button_pin;
+  Runner& runner;
+};
+
+class BehaviorNull: public Behavior {
+  void tick() {}
+};
+
+class BehaviorFollowLine: public Behavior {
+  void tick() {
+    const int val_l = digitalRead(sensor_pins.l);
+    const int val_c = digitalRead(sensor_pins.c);
+    const int val_r = digitalRead(sensor_pins.r);
+  }
+};
+
