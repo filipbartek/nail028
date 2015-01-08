5019ac3de44c04ea45b295e7417da4ace786ea88
Filip Bartek <filip.bartek@hotmail.com>
2015-01-08 01:12:13 +0100

    Add info comment to pins configuration

--- line/line.ino
+++ line/line.ino
@@ -5,6 +5,7 @@
 #include "Pins.h"
 
 // Configuration
+// Pins: {sensor[5], button, servo[2], diod[2]}
 const Pins pins_paulie = {{3, 4, 5, 6, 7}, 8, {13, 12}, {10, 9}};
 const Pins& pins = pins_paulie;
 const long velocity = 90;
